59387ffbdc16a203e6b536cfaf881e29170b754f
Andrew Dryga <andrew@dryga.com>
2016-08-01 18:41:39 +0300

    Restruct mix.exs and removed useless dependency

--- mix.exs
+++ mix.exs
@@ -1,26 +1,28 @@
 defmodule Multiverse.Mixfile do
   use Mix.Project
 
+  @version "0.3.0"
+
   def project do
     [app: :multiverse,
      description: "Manage version of your API via request and response gateways.",
      package: package,
-     version: "0.3.0",
+     version: @version,
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
      test_coverage: [tool: ExCoveralls],
-     preferred_cli_env: [coveralls: :test]]
+     preferred_cli_env: [coveralls: :test],
+     docs: [source_ref: "v#{@version}", main: "readme", extras: ["README.md"]]]
   end
 
   def application do
-    [applications: []]
+    [applications: [:timex, :plug]]
   end
 
   defp deps do
     [{:timex, "~> 3.0"},
-     {:cowboy, "~> 1.0"},
      {:plug, "~> 1.0"},
      {:ex_doc, ">= 0.0.0", only: [:dev, :test]},
      {:excoveralls, "~> 0.5", only: [:dev, :test]},
