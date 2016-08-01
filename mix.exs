9bb13e47a0f4a1fc24535cdfd6dd07f73f8fec29
Andrew Dryga <andrew@dryga.com>
2016-08-01 12:57:17 +0300

    Updated description

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Multiverse.Mixfile do
 
   def project do
     [app: :multiverse,
-     description: "API Gateway versioning plug",
+     description: "Manage version of your API via request and response gateways.",
      package: package,
      version: "0.1.0",
      elixir: "~> 1.3",
@@ -22,8 +22,8 @@ defmodule Multiverse.Mixfile do
     [{:timex, "~> 3.0"},
      {:cowboy, "~> 1.0"},
      {:plug, "~> 1.0"},
-     {:excoveralls, "~> 0.5", only: :test},
      {:ex_doc, ">= 0.0.0", only: :test},
+     {:excoveralls, "~> 0.5", only: [:dev, :test]},
      {:dogma, "~> 0.1", only: [:dev, :test]},
      {:credo, "~> 0.4", only: [:dev, :test]}]
   end
