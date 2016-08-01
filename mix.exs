77d75df95d43f700a526e99f65a16d492d7e73d6
Andrew Dryga <andrew@dryga.com>
2016-08-01 13:00:06 +0300

    Increment version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Multiverse.Mixfile do
     [app: :multiverse,
      description: "Manage version of your API via request and response gateways.",
      package: package,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -22,7 +22,7 @@ defmodule Multiverse.Mixfile do
     [{:timex, "~> 3.0"},
      {:cowboy, "~> 1.0"},
      {:plug, "~> 1.0"},
-     {:ex_doc, ">= 0.0.0", only: :test},
+     {:ex_doc, ">= 0.0.0", only: [:dev, :test]},
      {:excoveralls, "~> 0.5", only: [:dev, :test]},
      {:dogma, "~> 0.1", only: [:dev, :test]},
      {:credo, "~> 0.4", only: [:dev, :test]}]
