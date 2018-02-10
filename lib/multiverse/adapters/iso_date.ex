8a6261dbbb2a17f72c1d9425d6f3b287552ce80e
Andrew Dryga <andrew@dryga.com>
2018-02-10 18:11:50 +0200

    Force users to decide which version would be default one

--- lib/multiverse/adapters/iso_date.ex
+++ lib/multiverse/adapters/iso_date.ex
@@ -3,49 +3,105 @@ defmodule Multiverse.Adapters.ISODate do
   Adapter that fetches ISO-8601 date from request header and `Elixir.Date`
   to resolve changes that must be applied to the connection.
 
-  Current date is used as fallback when:
-    * version header is not present in request;
-    * value of a version header is malformed (and warning is logged);
-    * `latest` channel is used instead of date.
+  This adapter requires you to configure which version is used by default,
+  when value in a header was malformed or not set. It's configured via `:default_version`,
+  supported values:
 
-  When `edge` channel is used instead of date, no changes are applied to the connection.
+    * `:first` - apply all gates by default. This option is useful when you integrate Multiverse in existing project \
+    and API consumers are not ready to accept latest changes by default;
+    * `:latest` - user current date as default version. This option is useful when there are
+    no legacy clients or there was no breaking changes before those clients started to send API version.
+
+  ## Version Channels
+
+  Consumers can use two channels instead of date in version header:
+
+   * `latest` - apply only changes scheduled for future (with a date later than date when request arrived);
+   * `edge` - do not apply any changes.
   """
   require Logger
-
   @behaviour Multiverse.Adapter
+
+  @default_version_values [:latest, :oldest]
 
   @typep version :: Date.t()
 
-  def init(_adapter, opts), do: {:ok, opts}
+  @doc """
+  Initializes adapter configuration at compile time.
 
-  @spec version_comparator(v1 :: version, v2 :: version) :: boolean
+  Raises when `:default_version` option is not set.
+  """
+  def init(_adapter, opts) do
+    default_version = Keyword.get(opts, :default_version)
+
+    unless default_version do
+      raise ArgumentError, "Multiverse.Adapters.ISODate :default_version config is not set"
+    end
+
+    unless default_version in @default_version_values do
+      default_version_values_strings = Enum.map(@default_version_values, &inspect/1)
+
+      raise ArgumentError,
+            "invalid Multiverse.Adapters.ISODate :default_version config value, " <>
+              "possible values: #{Enum.join(default_version_values_strings, ", ")}, " <>
+              "got: #{inspect(default_version)}"
+    end
+
+    {:ok, opts}
+  end
+
+  @spec version_comparator(v1 :: version(), v2 :: version()) :: boolean
   def version_comparator("edge", _v2), do: false
   def version_comparator(v1, v2), do: Date.compare(v1, v2) == :lt
 
-  @spec fetch_default_version(conn :: Plug.Conn.t()) :: {:ok, version, Plug.Conn.t()}
-  def fetch_default_version(conn), do: {:ok, Date.utc_today(), conn}
-
-  @spec resolve_version_or_channel(conn :: Plug.Conn.t(), channel_or_version :: String.t()) :: {
-          :ok,
-          version,
-          Plug.Conn.t()
-        }
-  def resolve_version_or_channel(conn, "latest") do
-    fetch_default_version(conn)
+  @spec fetch_default_version(conn :: Plug.Conn.t(), adapter_config :: Multiverse.Adapter.config()) ::
+          {:ok, version, Plug.Conn.t()}
+  def fetch_default_version(conn, adapter_config) do
+    case Keyword.get(adapter_config, :default_version) do
+      :latest -> {:ok, Date.utc_today(), conn}
+      :oldest -> {:ok, fetch_oldest_version(adapter_config), conn}
+    end
   end
 
-  def resolve_version_or_channel(conn, "edge") do
+  defp fetch_oldest_version(adapter_config) do
+    with gates when length(gates) > 0 <- Keyword.fetch!(adapter_config, :gates) do
+      gates |> List.last() |> elem(0) |> Date.add(-1)
+    else
+      [] ->
+        :ok =
+          Logger.warn(fn ->
+            "[Multiverse] You specified default_version: :oldest but there are no gates, failing back to the current date"
+          end)
+
+        Date.utc_today()
+    end
+  end
+
+  @spec resolve_version_or_channel(
+          conn :: Plug.Conn.t(),
+          channel_or_version :: String.t(),
+          adapter_config :: Multiverse.Adapter.config()
+        ) :: {:ok, version, Plug.Conn.t()}
+  def resolve_version_or_channel(conn, "latest", _adapter_config) do
+    {:ok, Date.utc_today(), conn}
+  end
+
+  def resolve_version_or_channel(conn, "edge", _adapter_config) do
     {:ok, "edge", conn}
   end
 
-  def resolve_version_or_channel(conn, version) do
+  def resolve_version_or_channel(conn, version, adapter_config) do
     case Date.from_iso8601(version) do
       {:ok, date} ->
         {:ok, date, conn}
 
       {:error, reason} ->
-        :ok = Logger.warn("Malformed version header: `#{inspect(reason)}`, failing back to the default version")
-        fetch_default_version(conn)
+        :ok =
+          Logger.warn(fn ->
+            "[Multiverse] Malformed version header: #{inspect(reason)}, failing back to the default version"
+          end)
+
+        fetch_default_version(conn, adapter_config)
     end
   end
 end
